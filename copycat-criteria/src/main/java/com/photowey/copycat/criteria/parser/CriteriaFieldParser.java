c3a05b1e2766d0b710d42f4d4a67564e399984cd
photowey <photowey@gmail.com>
2021-12-05 00:35:53 +0800

    feat: add license file-header.

--- copycat-criteria/src/main/java/com/photowey/copycat/criteria/parser/CriteriaFieldParser.java
+++ copycat-criteria/src/main/java/com/photowey/copycat/criteria/parser/CriteriaFieldParser.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2019 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.photowey.copycat.criteria.parser;
 
 import com.photowey.copycat.criteria.annotaion.CriteriaQuery;
@@ -22,7 +37,7 @@ import java.util.stream.Collectors;
 public final class CriteriaFieldParser {
 
   private static final Map<Class<?>, List<Field>> CLASS_FIELD_REF_CACHE = new ConcurrentHashMap<>(
-      256);
+          256);
 
   private CriteriaFieldParser() {
     throw new AssertionError("No " + getClass().getName() + " instances for you!");
@@ -36,7 +51,7 @@ public final class CriteriaFieldParser {
    * @param <QUERY>  查询类型
    */
   public static <QUERY extends AbstractQuery<?>> void foreachCriteriaField(final QUERY query,
-      final CriteriaFieldCallback callback) {
+                                                                           final CriteriaFieldCallback callback) {
     List<Field> fieldList = CLASS_FIELD_REF_CACHE.get(query.getClass());
     if (fieldList == null) {
       synchronized (CLASS_FIELD_REF_CACHE) {
@@ -44,7 +59,7 @@ public final class CriteriaFieldParser {
         if (fieldList == null) {
           fieldList = new ArrayList<>();
           for (Class<?> clazz = query.getClass(); clazz != Object.class;
-              clazz = clazz.getSuperclass()) {
+               clazz = clazz.getSuperclass()) {
             final Field[] fields = clazz.getDeclaredFields();
             fieldList.addAll(Arrays.stream(fields).collect(Collectors.toList()));
           }
@@ -58,7 +73,7 @@ public final class CriteriaFieldParser {
   }
 
   private static void foreachCriteriaField(final List<Field> fieldList,
-      final CriteriaFieldCallback callback) {
+                                           final CriteriaFieldCallback callback) {
     for (final Field field : fieldList) {
       final Annotation[] annotations = field.getDeclaredAnnotations();
       for (final Annotation annotation : annotations) {
@@ -78,7 +93,7 @@ public final class CriteriaFieldParser {
    * @param callback 回调函数
    */
   private static void foreachCriteriaField(final Class<?> clazz,
-      final CriteriaFieldCallback callback) {
+                                           final CriteriaFieldCallback callback) {
     final Field[] fields = clazz.getDeclaredFields();
     for (final Field field : fields) {
       final Annotation[] annotations = field.getDeclaredAnnotations();
