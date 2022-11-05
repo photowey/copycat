1e6c9d76ce94dff39450062caa11adeb8ba9cae7
photowey <photowey@gmail.com>
2022-11-05 18:29:23 +0800

    feat: optimize @In and @NotIn

--- copycat-criteria/src/main/java/com/photowey/copycat/criteria/processor/NotInProcessor.java
+++ copycat-criteria/src/main/java/com/photowey/copycat/criteria/processor/NotInProcessor.java
@@ -16,13 +16,14 @@
 package com.photowey.copycat.criteria.processor;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.photowey.copycat.criteria.annotaion.ConditionProcessor;
 import com.photowey.copycat.criteria.annotaion.NotIn;
 import com.photowey.copycat.criteria.query.AbstractQuery;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Field;
-import java.util.List;
+import java.util.Collection;
 
 /**
  * {@link NotIn} 注解处理器
@@ -39,7 +40,7 @@ public class NotInProcessor<QUERY extends AbstractQuery, ENTITY>
     @Override
     public boolean process(QueryWrapper<ENTITY> queryWrapper, Field field, QUERY query, NotIn criteriaAnnotation) {
 
-        final List<Object> value = (List) this.columnValue(field, query);
+        final Collection<Object> value = (Collection) this.columnValue(field, query);
         if (this.isNullOrEmpty(value)) {
             // 属性值为 Null OR Empty 跳过
             return true;
@@ -50,7 +51,7 @@ public class NotInProcessor<QUERY extends AbstractQuery, ENTITY>
             columnName = this.columnName(field, criteriaAnnotation.naming());
         }
         assert columnName != null;
-        queryWrapper.notIn(null != value, columnName, value);
+        queryWrapper.notIn(ObjectUtils.isNotEmpty(value), columnName, value);
 
         return true;
     }
